c9c7c62110a7361700c3bff7f8d0bab74a083229
Artyom Lagun <artyom.lagun@gmail.com>
2018-06-05 13:22:51 +0300

    added update_request for facebook

--- lib/bottington/adapter/facebook.rb
+++ lib/bottington/adapter/facebook.rb
@@ -6,11 +6,37 @@ module Bottington
       end
 
       def update_request
+        @bot_request = @request.bot_request = Bottington::BotRequest.new
+        fb_response = @request['entry'][0]['messaging'][0]
 
+        @bot_request.user = Bottington::User.new(fb_response['sender']['id'], '', '', '')
+
+        msg = build_request_message(fb_response['message']['text'])
+        @bot_request.message = Bottington::Message.new(
+          fb_response['message']['mid'],
+          msg[:text],
+          msg[:type]
+        )
+        attachments = fb_response['message']['attachments']
+        @bot_request.media = Bottington::Media.new(
+          fb_response['message']['mid'],
+          attachments ? attachments['payload']['url'] : ''
+        )
+
+        @request
       end
 
       def response_body(bot_request, body, type)
-        {messaging_type: '', recipient: {id: ''}, message: {text: ''}}
+        {
+          messaging_type: 'RESPONSE',
+          recipient: {
+            id: bot_request.user.id
+          },
+          message: {
+            text: body
+            }
+          },
+          notification_type: 'REGULAR'
       end
 
       def platform_url
